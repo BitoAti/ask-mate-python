d98c4001c96a24f3c2259a9f279e2c723a2ae277
BitoAti <bitoati1987@gmail.com>
2020-01-09 14:53:36 +0100

    Registration:  -page                -validation                -route

--- server.py
+++ server.py
@@ -1,14 +1,26 @@
+import app as app
 from flask import Flask, session, redirect, url_for, escape, request, make_response, render_template, redirect, request, url_for
 import data_manager
 
-
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 
 @app.route('/')
 def index():
+    session.pop('user_name', None)
+    print(session)
     questions = data_manager.get_five_question()
     return render_template('index.html', question=questions)
+
+
+@app.route('/list')
+def list():
+    return render_template('list.html')
+
+
+
 
 
 @app.route('/registration', methods=['GET', 'POST'])
@@ -16,15 +28,14 @@ def registration():
     if request.method == 'POST':
         username = request.form.get('user_name')
         password = request.form.get("password")
-        print(username)
-        result = data_manager.check_username(username)
-        print(result, type(result[0]))
+        hashed_pw = data_manager.hash_password(password)
 
-        if result.num > 0:
-            print("22222222222")
-            hashed_pw = data_manager.hash_password(password)
+        try:
             data_manager.save_user_data(username,hashed_pw)
-        return redirect(url_for('/'))
+            session["user_name"] = username
+            return redirect('/list')
+        except:
+            return redirect(url_for('registration'))
     return render_template('registration.html')
 
 
